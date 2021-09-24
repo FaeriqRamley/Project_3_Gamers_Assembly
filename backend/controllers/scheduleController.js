077dd885c5a6aaa364af14f4f7b7a0df72dd3b8a
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-09-24 21:22:58 +0800

    game notifications backend complete

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -1,5 +1,8 @@
 const Schedule = require("../models/Schedule");
 const Invite = require("../models/Invite");
+const Timeslot = require("../models/Timeslot");
+const mongoose = require("mongoose");
+
 module.exports.createSchedule_post = async (req,res) => {
     await Schedule.create(
         {
@@ -16,28 +19,48 @@ module.exports.getSchedule_get = async (req,res) => {
 };
 
 module.exports.createTimeslotInvite_post = async (req,res) => {
-    
-    await Schedule.findOneAndUpdate({ownerId:req.body.senderId}, {$push:{sentNotifications:{...req.body, status: "Pending"}}});
-    await Schedule.findOneAndUpdate({ownerId:req.body.receiverId}, {$push:{receivedNotifications:{...req.body, status: "Pending"}}});
+    //Create Invite and save to invite db
+    const newInvite = new Invite(req.body);
+    await newInvite.save();
+    //Add reference to sender's sent notifications and receivers's received notifications
+    await Schedule.findOneAndUpdate({ownerId:req.body.senderId}, {$push:{sentNotifications: newInvite._id}});
+    await Schedule.findOneAndUpdate({ownerId:req.body.receiverId}, {$push:{receivedNotifications:newInvite._id}});
     res.json({status:"ok",message:"Successfully added"});
 }
 
 module.exports.respondTimeslotInvite_put = async (req,res) => {
 
-    // const senderSchedule = await Schedule.find({ownerId:req.body.senderId});
-    // const receiverSchedule = await Schedule.find({ownerId:req.body.receiverId});
-    // const senderIndex = senderSchedule.sentNotifications.findIndex((invite) => invite._id === req.body.inviteId);
-    // const receiverIndex = senderSchedule.sentNotifications.findIndex((invite) => invite._id === req.body.inviteId);
+    //These 3 lines is so that you don't have to input the senderId,receiverId,timeslotId from the front-end, only need input invite's id. Can remove also if yall want
+    const inviteObj = await Invite.findById(req.body.inviteId)
+    if (!inviteObj){res.json({status:"failed",message:"invite does not exist"})};
+    const {senderId,receiverId,timeslotId} = inviteObj;
+    const {inviteId} = req.body;
 
     if (req.params.response === "Decline"){
-        await Schedule.updateOne(
-            { "ownerId":req.body.senderId, "sentNotifications._id": req.body.inviteId },
-            { $set: {"sentNotifications.$.status":"Declined"}}
-        )
-        res.json({status:"ok",message:"successfully declined"})
+        //Change invite's status to "Declined"
+        await Invite.findByIdAndUpdate(inviteId,{$set:{status:"Declined"}});
+        //Remove invite from receiver's received notifications array (cause he's already seen it)
+        await Schedule.findOneAndUpdate({ownerId:receiverId},{$pull:{receivedNotifications:inviteId}})
+        res.json({status:"ok",message:"successfully declined"});
+
     } else if (req.params.response === "Accept"){
-        res.json({status:"ok",message:"successfully accepted"})
+        //Change Invite's status to "Declined"
+        await Invite.findByIdAndUpdate(inviteId,{$set:{status:"Accepted"}});
+        //Add timeslot reference to sender's timeslots
+        await Schedule.findOneAndUpdate( {ownerId:senderId},{$push:{timeslots:mongoose.Types.ObjectId(timeslotId)}})
+        //Add sender's ID to the timeslot's attendees
+        await Timeslot.findByIdAndUpdate(timeslotId,{$push:{attendees:senderId.toString()}})
+        //Remove invite from receiver's received notifications array (cause he's already seen it)
+        await Schedule.findOneAndUpdate({ownerId:receiverId},{$pull:{receivedNotifications:inviteId}})
+        res.json({status:"ok",message:"successfully accepted"});
     } else {
         res.json({status:"failed",message:"Response not recognized"})
     }
 }
+
+//For sender who has already read the returned notification to remove it from their history (and invite db, idk if we wanna do this or not though)
+module.exports.removeRespondedTimeslotInvite_delete = async (req,res) => {
+    await Schedule.findOneAndUpdate({ownerId:req.body.senderId},{$pull:{sentNotifications:req.body.inviteId}});
+    await Invite.findByIdAndDelete(req.body.inviteId);
+    res.json({status:"ok",messaged:"Removed invite from db and reference from sender's schedule"})
+} 
--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const router = express.Router();
-const {getSchedule_get,createSchedule_post,createTimeslotInvite_post,respondTimeslotInvite_put} = require("../controllers/scheduleController");
+const {
+    getSchedule_get,
+    createSchedule_post,
+    createTimeslotInvite_post,
+    respondTimeslotInvite_put,
+    removeRespondedTimeslotInvite_delete
+} = require("../controllers/scheduleController");
 
 router.get("/",getSchedule_get);
 router.post("/",createSchedule_post);
@@ -9,4 +15,5 @@ router.get("/populate/timeslot",()=>{});
 // Notification Routes
 router.post("/createInvite",createTimeslotInvite_post);
 router.put("/respondInvite/:response",respondTimeslotInvite_put);
+router.delete("/clearInvite",removeRespondedTimeslotInvite_delete)
 module.exports = router;
