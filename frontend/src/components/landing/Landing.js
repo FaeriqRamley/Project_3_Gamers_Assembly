b8853cf92837070a42a4690279558d23b508f08a
jerroldtkl <jerroldtkl@gmail.com>
2021-09-27 00:40:47 +0800

    created landing

--- frontend/src/components/landing/Landing.js
+++ frontend/src/components/landing/Landing.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import { connect } from "react-redux"
+import React from "react";
+import { connect } from "react-redux";
+import { Row, Col } from "antd";
+import "antd/dist/antd.css";
+import Carousel from "./carousel";
+import FeaturedUsers from "./featuredUsers";
+import styles from "./landing.css";
 
 function Landing(props) {
-    console.log(props.auth)
-    return (
-        <div>
-            This is Landing
-        </div>
-    )
+  console.log(props.auth);
+  return (
+    <React.Fragment>
+      <div className="container">
+        <Row>
+          <Col span={8}></Col>
+          <Col span={8} className="site-container">
+            GamersAssemble
+          </Col>
+          <Col span={8}></Col>
+        </Row>
+      </div>
+      <div className="carousel-container">
+        <Carousel></Carousel>
+      </div>
+      <FeaturedUsers />
+    </React.Fragment>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        auth: state.auth
-    }
-}
+  return {
+    auth: state.auth,
+  };
+};
 
-export default connect(mapStateToProps)(Landing)
+export default connect(mapStateToProps)(Landing);
---
+++ frontend/src/components/landing/featuredUsers.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Row, Col, Button, ToolTip } from "antd";
+
+import "antd/dist/antd.css";
+import styles from "./landing.css";
+import UserCard from "../userdetails/userCard";
+
+function featuredUsers() {
+  let content = (
+    <Col className="featured-col" span={4}>
+      <button>
+        <ToolTip placement="bottom" title={<UserCard />}>
+          <img
+            className="featured-img"
+            src="https://images.unsplash.com/photo-1566650554919-44ec6bbe2518?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwYW5pbWFsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+            alt=""
+            type="button"
+            onClick=""
+          />
+        </ToolTip>
+      </button>
+    </Col>
+  );
+  return (
+    <React.Fragment>
+      <div className="featured-container">
+        <Row gutter={16}>
+          {content}
+          {content}
+          {content}
+          {content}
+          {content}
+          {content}
+        </Row>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default featuredUsers;
