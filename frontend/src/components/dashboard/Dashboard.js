d0694525638d8419f209c88504148a6f49c66404
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-09-26 17:05:34 +0800

    created layout of dashboard

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Layout} from 'antd';
 
+const {Content,Sider} = Layout
 export default function Dashboard() {
+
+    const siderStyle = {
+        backgroundColor:"rgba(0,0,0,0.2)",
+        minWidth:"300px"
+    }
+
+
     return (
-        <div>
-            This is Dashboard
-        </div>
+        <Layout style={{backgroundColor:"rgba(0,0,0,0)"}}>
+            <Sider style={siderStyle} breakpoint="lg" collapsedWidth="0" width={"12vw"}>
+                Friends
+            </Sider>
+            <Content style={{minHeight:"90vh"}}>
+                Content
+            </Content>
+            <Sider style={siderStyle} breakpoint="lg" collapsedWidth="0" trigger={null} width={"16vw"}>
+                Notifications
+            </Sider>
+        </Layout>
     )
 }
