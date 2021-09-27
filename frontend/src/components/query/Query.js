4f02a03a718252438a26c96b8d04eef83261b6d3
jerroldtkl <jerroldtkl@gmail.com>
2021-09-27 14:34:36 +0800

    commit minor changes

--- frontend/src/components/query/Query.js
+++ frontend/src/components/query/Query.js
@@ -1,61 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Card, Input } from 'semantic-ui-react'
-import UserCard from "/Users/liewjinyao/Documents/GACOURSE/Project3/Project_3_Gamers_Assembly/frontend/src/components/userdetails/userCard.js";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card, Input } from "semantic-ui-react";
 
 export default function Query() {
-    const [APIData, setAPIData] = useState([])
-    const [filteredResults, setFilteredResults] = useState([]);
-    const [searchInput, setSearchInput] = useState('');
-    useEffect(() => {
-        axios.get(`http://localhost:5000/api/users/all/50`)
-            .then((response) => {
-                setAPIData(response.data);
-            })
-    }, [])
+  const [APIData, setAPIData] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/users/all/50`).then((response) => {
+      setAPIData(response.data);
+    });
+  }, []);
 
-    const searchItems = (searchValue) => {
-        setSearchInput(searchValue)
-        if (searchInput !== '') {
-            const filteredData = APIData.filter((item) => {
-                return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
-            })
-            setFilteredResults(filteredData)
-        }
-        else {
-            setFilteredResults(APIData)
-        }
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = APIData.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(APIData);
     }
+  };
 
-    return (
-        <div style={{ padding: 20 }}>
-            <Input icon='search'
-                placeholder='Search...'
-                onChange={(e) => searchItems(e.target.value)}
-            />
-            <Card.Group itemsPerRow={3} style={{ marginTop: 20 }}>
-                {searchInput.length > 1 ? (
-                    filteredResults.map((item) => {
-                        return (
-                            <Card>
-                                <Card.Content>
-                                    <Card.Header>{item.userName}</Card.Header>
-                                    <Card.Description>
-                                        {item.email}
-                                    </Card.Description>
-                                </Card.Content>
-                            </Card>
-                        )
-                    })
-                ) : (
-                    APIData.map((item) => {
-                        return (
-                            <UserCard data={item} />
-                        )
-                    })
-                )}
-            </Card.Group>
-        </div>
-    )
+  return (
+    <div style={{ padding: 20 }}>
+      <Input
+        icon="search"
+        placeholder="Search..."
+        onChange={(e) => searchItems(e.target.value)}
+      />
+      <Card.Group itemsPerRow={3} style={{ marginTop: 20 }}>
+        {searchInput.length > 1
+          ? filteredResults.map((item) => {
+              return (
+                <Card>
+                  <Card.Content>
+                    <Card.Header>{item.userName}</Card.Header>
+                    <Card.Description>{item.email}</Card.Description>
+                  </Card.Content>
+                </Card>
+              );
+            })
+          : APIData.map((item) => {
+              return <UserCard data={item} />;
+            })}
+      </Card.Group>
+    </div>
+  );
 }
-
