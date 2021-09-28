d845f4af180193be03fefed255d7f0504623e232
bryanboey <bryanboey@gmail.com>
2021-09-28 09:16:19 +0800

    Update Landing.js

--- frontend/src/components/landing/Landing.js
+++ frontend/src/components/landing/Landing.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { Row, Col } from "antd";
 import "antd/dist/antd.css";
 import Carousel from "./carousel";
-import FeaturedUsers from "./featuredUsers";
+import FeaturedUsers from "./FeaturedUsers";
 import "./landing.css";
 
 function Landing(data) {
