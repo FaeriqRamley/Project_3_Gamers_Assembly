fcc426a05a42b52d55bc92f891b6337cdc4b8744
jerroldtkl <jerroldtkl@gmail.com>
2021-09-29 16:28:42 +0800

    completed landing(left site pics)

--- frontend/src/components/landing/MyCarousel.js
+++ frontend/src/components/landing/MyCarousel.js
@@ -1,41 +1,50 @@
 import React from "react";
-import { Carousel } from "antd";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "antd/dist/antd.css";
-// import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 function MyCarousel() {
-  const contentStyle = {
-    height: "160px",
-    color: "#fff",
-    lineHeight: "160px",
-    textAlign: "center",
-  };
-
-  // const RightArrow = () => {
-  //   return <Button icon={<RightOutlined />} size="middle" />;
-  // };
-
-  // const LeftArrow = () => {
-  //   return <Button icon={<LeftOutlined />} size="middle" />;
-  // };
-
   return (
     <React.Fragment>
-      <Carousel autoplay effect="fade">
-        <div>
-          <h3 style={contentStyle}>1</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>3</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>4</h3>
-        </div>
-      </Carousel>
+      <div>
+        <Carousel
+          dynamicHeight={true}
+          autoPlay
+          infiniteLoop
+          showArrows={true}
+          stopOnHover={false}
+          showThumbs={false}
+        >
+          <div>
+            <img
+              style={{ height: "500px", objectFit: "cover" }}
+              src="https://venturebeat.com/wp-content/uploads/2020/12/discord.jpg?w=1200&strip=all"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              style={{ height: "500px", objectFit: "cover" }}
+              src="https://techcrunch.com/wp-content/uploads/2021/03/Invited.png?w=680"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              style={{ height: "500px", objectFit: "cover" }}
+              src="https://preview.redd.it/x9vsiutqh2z51.png?width=2880&format=png&auto=webp&s=9c3c788db30a718f0a2c11a3eb048457daf422e1"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              style={{ height: "500px", objectFit: "cover" }}
+              src="https://www.gannett-cdn.com/presto/2019/08/09/USAT/b1d25399-ae60-498c-b959-7e778ea30fa0-DISCORD-go-live-spectating.png"
+              alt=""
+            />
+          </div>
+        </Carousel>
+      </div>
     </React.Fragment>
   );
 }
-
 export default MyCarousel;
